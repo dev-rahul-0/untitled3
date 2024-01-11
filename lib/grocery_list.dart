d63474876c9a28b33c98559697de230be4475356
dev-rahul-0 <rs650417@gmail.com>
2024-01-11 23:36:48 +0530

    update

--- lib/grocery_list.dart
+++ lib/grocery_list.dart
@@ -8,7 +8,7 @@ class GroceryList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Groceries'),
+        title: const Text('Your Groceries'),
       ),
       body: ListView.builder(
           itemCount: groceryItems.length,
